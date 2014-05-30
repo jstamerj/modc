23be82651334f2166f8fca89520a2886048186c2
Jeff Stamerjohn <Jeff.Stamerjohn@hbo.com>
2014-05-30 15:43:54 -0700

    add debugging

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var value = null
 
+console.log(__dirname, 'Loading modc');
+
 module.exports.set = function(newValue) {
- value = newValue;
+  console.log(__dirname, 'Setting value...', newValue);
+  value = newValue;
 }
 
 module.exports.get = function() {
+  console.log(__dirname, 'Getting value...', value);
   return value;
 }
